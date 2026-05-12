4a40454597371a5bfac97e96b85afb528ea59c31
Adandu <adandu@adandu.ro>
2026-05-12 18:32:10 +0300

    feat: add LivePageRefresh client component

---
+++ src/components/live-page-refresh.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export function LivePageRefresh() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const interval = setInterval(() => router.refresh(), 60_000)
+    return () => clearInterval(interval)
+  }, [router])
+
+  return null
+}
